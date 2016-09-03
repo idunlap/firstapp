985ca3e82d0ebd07972dbee7571d07938758b119
peter dunlap <idunlap@mail.com>
2016-09-03 13:51:20 -0700

    Addedmore files for stripe

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,11 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
 get 'visitors/index' => 'visitor#index'
 
-  ######get '/about', to: 'pages#about'
+  #get '/about', to: 'pages#about'
+
+
+#Used from Stripe.com for payment processies
+  resources :charges
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
